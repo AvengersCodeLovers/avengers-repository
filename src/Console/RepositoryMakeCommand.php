87382969bac3b79b063e72b3f26e8a18187e2b51
huusu1996@gmail.com <huusu1996@gmail.com>
2020-07-22 16:12:41 +0700

    Make command repository

---
+++ src/Console/RepositoryMakeCommand.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Avengers\Console;
+
+use Illuminate\Support\Str;
+use Illuminate\Console\GeneratorCommand;
+use Symfony\Component\Console\Input\InputOption;
+
+class RepositoryMakeCommand extends GeneratorCommand
+{
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'make:repository';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new repository class';
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        if (parent::handle() === false && ! $this->option('force')) {
+            return;
+        }
+
+        if ($this->option('interface')) {
+            $this->createRepositoryInterface();
+        }
+    }
+
+    protected function createRepositoryInterface()
+    {
+        $repositoryName= Str::studly(class_basename($this->argument('name')));
+
+        $this->call('make:interface', [
+            'name' => "{$repositoryName}Interface",
+        ]);
+    }
+
+    /**
+     * The type of class being generated.
+     *
+     * @var string
+     */
+    protected $type = 'Repository';
+
+    /**
+     * Get the stub file for the generator.
+     *
+     * @return string
+     */
+    protected function getStub()
+    {
+        return __DIR__ . '/stubs/repository.stub';
+    }
+
+    /**
+     * Get the default namespace for the class.
+     *
+     * @param  string  $rootNamespace
+     * @return string
+     */
+    protected function getDefaultNamespace($rootNamespace)
+    {
+        return $rootNamespace . '\Repositories';
+    }
+
+    /**
+     * Get the console command options.
+     *
+     * @return array
+     */
+    protected function getOptions()
+    {
+        return [
+            ['force', 'f', InputOption::VALUE_NONE, 'Create the class even if the repository already exists.'],
+            ['interface', 'i', InputOption::VALUE_NONE, 'Create a new interface for the repository.'],
+        ];
+    }
+}
